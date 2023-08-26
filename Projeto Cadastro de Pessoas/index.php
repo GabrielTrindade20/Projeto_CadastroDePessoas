ca2c65ff10a85f08b4a8c0d72d1155650b80daa8
Gabriel <gabriel.rochat18@gmai.com>
2023-08-25 23:24:31 -0300

    criação html e css

--- Projeto Cadastro de Pessoas/index.php
+++ Projeto Cadastro de Pessoas/index.php
@@ -4,13 +4,15 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../style.css">
+    <link rel="stylesheet" href="./style.css">
     <title>Página de Cadastro</title>
 </head>
 
 <body>
     <section id="esquerda">
         <form action="">
+            <h2>CADASTRAR PESSOA</h2>
+
             <label for="nome">Nome</label>
             <input type="text" name="nome" id="nome">
 
@@ -24,22 +26,20 @@
         </form>
     </section>
 
-    <div>
-        olá
-    </div>
 
     <section id="direita">
         <table>
-            <tr>
+            <tr id="titulo">
                 <td>Nome</td>
                 <td>Telefone</td>
-                <td>Email</td>
+                <td colspan="2">Email</td>
             </tr>
 
             <tr>
                 <td>Gabriel</td>
                 <td>61 86549-6545</td>
                 <td>gabriel@gmail</td>
+                <td><a href="Editar">Editar</a><a href="Excluir">Excluir</a></td>
             </tr>
         </table>
     </section>
